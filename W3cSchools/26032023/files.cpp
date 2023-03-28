9d0e79568e538baef873c39d140c0548410375b0
nucleartachyon <armanpanchal03.ap@gmail.com>
2023-03-28 09:28:23 +0530

    commit 3.5

---
+++ W3cSchools/26032023/files.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main(){
+    string mytext;
+
+    ofstream MyFile("name.txt");
+
+    MyFile<<"files can be tricky, but fun enough";
+
+    MyFile.close();
+
+    ifstream readingFile("name.txt");
+
+    while (getline(readingFile, mytext)){
+        cout<<mytext;
+    }
+}
