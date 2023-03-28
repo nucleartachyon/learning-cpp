8317704956dea4b4d672569e14668cd9dc0717a7
nucleartachyon <armanpanchal03.ap@gmail.com>
2023-03-28 08:35:56 +0530

    commit3.2

--- W3cSchools/26032023/functions.cpp
+++ W3cSchools/26032023/functions.cpp
@@ -4,6 +4,16 @@ using namespace std;
 void myFn(){
     cout<<"i just got executed \n";
 }
+
+void MultiParam(string fName, int age){
+    cout<<"your name is "<<fName<<" and "<<" your age is "<<age<<"\n";
+}
+
 int main(){
     myFn();
+    string fName = "Aryan";
+    int age = 19;
+    MultiParam(fName,age);
+    MultiParam("harsh",19);
+    return 0;
 }
