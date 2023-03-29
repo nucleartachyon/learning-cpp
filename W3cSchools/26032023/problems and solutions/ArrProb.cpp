53cd6f281e30331891a69f5de13c8b4aec8351df
nucleartachyon <armanpanchal03.ap@gmail.com>
2023-03-29 09:19:24 +0530

    commit 4.2

---
+++ W3cSchools/26032023/problems and solutions/ArrProb.cpp
@@ -0,0 +1,11 @@
+/*
+problem: you are given an array of size 12.
+        write a program to print only the first,fifth and last letters of the array sequentially        
+*/
+#include <iostream>
+using namespace std;
+
+int main(){
+    char Words[] = {'c','h','a','m','p','i','o','n','s','h','i','p'};
+    cout<<Words[0]<<","<<Words[4]<<","<<Words[11]<<"\n";
+}
---
+++ W3cSchools/26032023/problems and solutions/discounts.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    double ptOf;
+    double items[] = {500,12.4,94,45,3,81,1000.9,85,90,1,35};
+    cout<<"input percent of:";
+    cin>>ptOf;
+    for(int i : items){
+        
+    }
+}
