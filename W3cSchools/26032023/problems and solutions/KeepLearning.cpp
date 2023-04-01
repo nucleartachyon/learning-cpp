30eaaf10c13573c4597f4c8f791249da10fa5983
nucleartachyon <armanpanchal03.ap@gmail.com>
2023-04-01 10:41:30 +0530

    commit 7.4

---
+++ W3cSchools/26032023/problems and solutions/KeepLearning.cpp
@@ -0,0 +1,15 @@
+/*
+problem: Complete the function in order to print the sentence "Keep learning!" and call it from main() function.
+*/
+
+#include <iostream>
+using namespace std;
+
+void printsentence(){
+    cout<<"Keep learning!";
+}
+
+int main(){
+    printsentence();
+    return 0;
+}
