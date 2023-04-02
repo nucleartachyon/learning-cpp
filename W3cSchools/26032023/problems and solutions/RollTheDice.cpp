4df20f905b9e9a263e9a6f214374a40b6130d249
nucleartachyon <armanpanchal03.ap@gmail.com>
2023-04-02 10:50:29 +0530

    commit 8.2

---
+++ W3cSchools/26032023/problems and solutions/RollTheDice.cpp
@@ -0,0 +1,30 @@
+/*
+problem: While playing a board game, you and your friend roll the dice and the person who rolled the higher number moves forward.
+         Write a function that takes two numbers as arguments and returns the higher number of the two. Print the result.
+         If numbers are equal function must return that number.
+*/
+#include <iostream>
+using namespace std;
+
+int max(int num1, int num2) {
+    if(num1==num2){
+        return num1;
+    }else if(num1>num2){
+        return num1;
+    }else{
+        return num2;
+    }
+}
+
+int main() {
+    //getting inputs
+    int first;
+    cin >> first;
+    int second;
+    cin >> second;
+    cout<<max(first,second);
+    //call the function and print result
+    
+    
+    return 0;
+}
